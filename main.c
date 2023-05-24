4c819dcd9ce427aa4384da349b34ead0879dbf4a
imamyah <imanemachkour8@gmail.com>
2023-05-24 23:45:46 +0000

    main function

--- main.c
+++ main.c
@@ -1,50 +1,100 @@
 #include "shell.h"
 
-/**
- * main - creates a simple shell
- *
- * Return: 0
- */
+char *_getenv(const char *env_var)
+{
+	extern char **environ;
+	int i = 0;
+	char *key;
 
-int main(void)
+	while (environ[i])
+	{
+		key = strtok(environ[i], "=");
+		if (strcmp(env_var, key) == 0)
+			return (strtok(NULL, "\n"));
+		i++;
+	}
+	return (NULL);
+}
+
+char *get_path(char *command)
+{
+	char *path = _getenv("PATH");
+	char *token;
+	char *command_path;
+
+	token = strtok(path, ":");
+	while (token)
+	{
+		command_path = malloc(strlen(token) + strlen(command) + 2);
+		strcpy(command_path, token);
+		strcat(command_path, "/");
+		strcat(command_path, command);
+		if (access(command_path, X_OK) == 0)
+			return (command_path);
+		free(command_path);
+		token = strtok(NULL, ":");
+	}
+	return (0);
+}
+
+char **get_string(char *buffer, char *delim)
+{
+	char **arrays;
+	char *arg;
+	int argc = 0;
+
+	arrays = malloc(sizeof(char *) * 1024);
+	arg = strtok(buffer, delim);
+	while (arg)
+	{
+		arrays[argc] = arg;
+		arg = strtok(NULL, delim);
+		argc++;
+	}
+	arrays[argc] = NULL;
+	return (arrays);
+}
+
+int main(int ac, char **av, char **env)
 {
 	size_t buffer_size = 0;
-	char **array, *arg, *buffer = NULL, *command;
-	int nread, status, argc;
+	char **token, **array, *buffer = NULL,  *command;
+	int status, nread;
 	pid_t pid;
+	(void) ac;
+	(void) av;
 
 	while (1)
 	{
-		write(1, "$ ", 2);
+		write (1, "> ", 2);
 		nread = getline(&buffer, &buffer_size, stdin);
-		if (nread == EOF)
-			exit(1);
-		buffer[nread - 1] = '\0';
-		if (strcmp(buffer, "exit") == 0)/* exit status */
-			exit_shell();
-		array = malloc(sizeof(char *) * 1024);
-		argc = 0;/* Tokenize the command line arguments */
-		arg = strtok(buffer, " ");
-		while (arg != NULL)
+		if (nread == -1)
 		{
-			array[argc++] = arg;
-			arg = strtok(NULL, " ");
+			write(1, "\n", 1);
+			exit (1);
 		}
-		array[argc] = NULL;
+		buffer[nread - 1] = '\0';
+		array = malloc(sizeof(char *) * 1024);
+		*array = buffer;
+		execve(array[0], array, NULL);
+
+		token = get_string(buffer, " \t\n");
+		if (strcmp(token[0], "exit") == 0)
+			exit(0);
+
+		
 		pid = fork();
 		if (pid == 0)
-		{/* Checks if the command exits in the PATH */
-			command = get_path(array[0]);
+		{
+			command = get_path(token[0]);
 			if (command)
-				execve(array[0], array, NULL);
+				execve(command, token, env);
 			else
-				perror("Command not found");
-			exit(1);
+				printf("Command not found\n");
+			exit(0);
 		}
 		else
 			wait(&status);
-		free(array);
 	}
-	splitter(buffer);
 	return (0);
 }

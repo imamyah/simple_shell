490fd7dfc151a69fcd55f163b9c5249f2f88d9d6
anas-hfd <anas1.hfd@gmail.com>
2023-05-24 10:18:11 -0700

    header file

---
+++ shell.h
@@ -0,0 +1,28 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+/* libraries */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+/* ---------- */
+
+
+/* functions */
+/*task 2*/
+char **splitter(char *str);
+int handle_args(void);
+/*task 3*/
+char *get_path(char *command);
+/*task 4*/
+void exit_shell(void);
+/*task 5*/
+char *_getenv(const char *env_name);
+
+/* ---------- */
+
+#endif

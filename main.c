ccccb0996a699a6ea34ff492a6c7db831925e3e7
imamyah <imanemachkour8@gmail.com>
2023-05-25 01:45:57 +0000

    main

--- main.c
+++ main.c
@@ -1,41 +1,11 @@
 #include "shell.h"
 
-char *_getenv(const char *env_var)
-{
-	extern char **environ;
-	int i = 0;
-	char *key;
-
-	while (environ[i])
-	{
-		key = strtok(environ[i], "=");
-		if (strcmp(env_var, key) == 0)
-			return (strtok(NULL, "\n"));
-		i++;
-	}
-	return (NULL);
-}
-
-char *get_path(char *command)
-{
-	char *path = _getenv("PATH");
-	char *token;
-	char *command_path;
-
-	token = strtok(path, ":");
-	while (token)
-	{
-		command_path = malloc(strlen(token) + strlen(command) + 2);
-		strcpy(command_path, token);
-		strcat(command_path, "/");
-		strcat(command_path, command);
-		if (access(command_path, X_OK) == 0)
-			return (command_path);
-		free(command_path);
-		token = strtok(NULL, ":");
-	}
-	return (0);
-}
+/**
+ * get_string - get commands
+ * @buffer: buffer
+ * @delim: delimeter
+ * Return: str
+ */
 
 char **get_string(char *buffer, char *delim)
 {
@@ -55,43 +25,47 @@ char **get_string(char *buffer, char *delim)
 	return (arrays);
 }
 
+/**
+ * main - main function
+ * @ac: arg count
+ * @av: arg vector
+ * @env: environ
+ * Return: int
+ */
+
 int main(int ac, char **av, char **env)
 {
 	size_t buffer_size = 0;
-	char **token, **array, *buffer = NULL,  *command;
+	char **token, *buffer = NULL;
 	int status, nread;
 	pid_t pid;
 	(void) ac;
 	(void) av;
+	(void) env;
 
 	while (1)
 	{
-		write (1, "> ", 2);
+		write(1, "$ ", 2);
 		nread = getline(&buffer, &buffer_size, stdin);
 		if (nread == -1)
 		{
 			write(1, "\n", 1);
-			exit (1);
+			exit(1);
 		}
 		buffer[nread - 1] = '\0';
-		array = malloc(sizeof(char *) * 1024);
-		*array = buffer;
-		execve(array[0], array, NULL);
 
 		token = get_string(buffer, " \t\n");
 		if (strcmp(token[0], "exit") == 0)
 			exit(0);
 
-		
 		pid = fork();
 		if (pid == 0)
 		{
-			command = get_path(token[0]);
-			if (command)
-				execve(command, token, env);
-			else
+			if (execve(token[0], token, NULL) == -1)
+			{
 				printf("Command not found\n");
-			exit(0);
+				exit(0);
+			}
 		}
 		else
 			wait(&status);

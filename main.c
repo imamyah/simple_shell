ffeb9b25082057f445596a207250aa57d7b8f044
imamyah <imanemachkour8@gmail.com>
2023-05-25 01:57:11 +0000

    main

--- main.c
+++ main.c
@@ -6,10 +6,7 @@
  * @delim: delimeter
  * Return: str
  */
-<<<<<<< HEAD
 
-=======
->>>>>>> 46355c796c18ca3762513a2c267d7a9db307b62f
 char **get_string(char *buffer, char *delim)
 {
 	char **arrays;
@@ -30,15 +27,7 @@ char **get_string(char *buffer, char *delim)
 /* -------- get string ----- */
 
 /**
- * main - main function
- * @ac: arg count
- * @av: arg vector
- * @env: environ
- * Return: int
- */
-
-/**
- * main - main function
+ * main - simple shell
  * @ac: arg count
  * @av: arg vector
  * @env: environ
@@ -57,11 +46,8 @@ int main(int ac, char **av, char **env)
 
 	while (1)
 	{
-<<<<<<< HEAD
 		write(1, "$ ", 2);
-=======
 		write(1, "> ", 2);
->>>>>>> 46355c796c18ca3762513a2c267d7a9db307b62f
 		nread = getline(&buffer, &buffer_size, stdin);
 		if (nread == -1)
 		{
@@ -69,30 +55,14 @@ int main(int ac, char **av, char **env)
 			exit(1);
 		}
 		buffer[nread - 1] = '\0';
-<<<<<<< HEAD
-
-=======
->>>>>>> 46355c796c18ca3762513a2c267d7a9db307b62f
 		token = get_string(buffer, " \t\n");
 		if (strcmp(token[0], "exit") == 0)
 			exit(0);
-
 		pid = fork();
 		if (pid == 0)
 		{
-<<<<<<< HEAD
 			if (execve(token[0], token, NULL) == -1)
 			{
-=======
-			array = malloc(sizeof(char *) * 1024);
-			*array = buffer;
-			execve(array[0], array, NULL);
-
-			command = get_path(token[0]);
-			if (command)
-				execve(command, token, env);
-			else
->>>>>>> 46355c796c18ca3762513a2c267d7a9db307b62f
 				printf("Command not found\n");
 				exit(0);
 			}
